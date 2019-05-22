396d43378cdff0701494d1b71a50064b8efc45e4
datata <danitt15@gmail.com>
2019-05-22 13:09:33 +0200

    movie detail working and styled

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import { ApiMoviesService } from '../api-movies.service';
 
 @Component({
   selector: 'app-movie-detail',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailComponent implements OnInit {
 
-  constructor() { }
+  id:string;
+  movieData:object;
+  similarMovies:object[]
+
+
+  constructor(private route:ActivatedRoute, private api:ApiMoviesService) { }
 
   ngOnInit() {
-  }
 
+    this.route.params.subscribe(params =>{
+          window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+
+          this.api.getMovie(params.id).subscribe((res:object)=>{
+            console.log(res);
+            this.movieData=res;
+          });
+
+          this.api.getSimilar(params.id).subscribe((res:any)=>{
+            console.log(res);
+            this.similarMovies=res.results;
+          })
+
+
+
+    })
+  }
 }
